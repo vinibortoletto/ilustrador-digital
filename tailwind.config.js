acd287475d52080a8257837353c044c7dc82028c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2019-12-18 20:32:39 -0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      base: ["Quicksand", "sans-serif"],
+      title: ["Belgrano", "serif"]
+    },
+    extend: {
+      width: {
+        dotsMobile: "320px",
+        dotsDesktop: "500px"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
